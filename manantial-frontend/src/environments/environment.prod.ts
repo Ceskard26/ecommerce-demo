21f3dd10b32473580adbc6fe617b3af93efbaf66
Ceskad26 <cesaracarrascor1026@gmail.com>
2026-07-15 09:05:37 -0500

    Fix production build to use environment.prod.ts and point apiUrl to Beanstalk

--- manantial-frontend/src/environments/environment.prod.ts
+++ manantial-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // Reemplaza esto por el DNS real del ALB de Elastic Beanstalk una vez desplegado,
-  // ej: http://manantial-ecommerce-env.us-east-1.elasticbeanstalk.com/api
-  apiUrl: 'http://REEMPLAZAR-CON-DNS-DEL-ALB.us-east-1.elasticbeanstalk.com/api',
+  apiUrl: 'http://Manantial-ecommerce-env.eba-kdukk3it.us-east-1.elasticbeanstalk.com/api',
 };
